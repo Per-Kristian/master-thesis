310228e682b50b36934b857add5d7f35f483c372
Per-Kristian <perkristian.nilsen@gmail.com>
2018-05-17 23:04:55 +0200

    Added a quick copied function for looping analysis over night.

---
+++ combined/functions/runCombSALoop.m
@@ -0,0 +1,53 @@
+function runCombSALoop(user, imposter, probeSets, setType, monoRefs, ...
+	diRefs, fast, paramNote, resultNote)
+%RUNCOMB Summary of this function goes here
+%   Detailed explanation goes here
+%params.CA.rwrdThreshold = 1.3;
+%params.CA.tolerance = NaN;
+%params.CA.singleOccScore = 2.45;
+%params.CA.missingScore = 2.75;
+params.CA.rwrdThreshold = NaN;
+params.CA.tolerance = 0.5;
+params.CA.width = 0.28;
+params.CA.singleOccScore = 5;
+params.CA.missingScore = 5;
+params.CA.maxRwrd = 1;
+params.CA.maxPen = 1;
+params.CA.lockout = 50;
+params.CA.type = 'simpleSMD';
+params.CA.note = '';
+
+params.PA.blockLength = 250;
+params.PA.absThresh = 1.25;
+params.PA.tolerance = 0.5;
+params.PA.note = 'Half weights for digraphs';
+
+params.infl.type = 'decisionLevel';
+params.infl.upMult = 1.001; % remember to remove breakpoint
+params.infl.downMult = 0.8;
+params.infl.rwrdThreshold = NaN;
+params.infl.tolerance = NaN;
+params.infl.width = NaN;
+params.infl.maxPen = NaN;
+
+%params.infl.type = 'scoreLevel';
+%params.infl.upMult = NaN;
+%params.infl.downMult = NaN;
+%params.infl.rwrdThreshold = NaN;
+%params.infl.tolerance = 0.8;
+%params.infl.width = 0.1;
+%params.infl.maxPen = 100 - params.CA.lockout + 0.001;
+
+params.infl.note = paramNote;
+
+runner = CombRunner(user, imposter, params, probeSets, setType, ...
+	monoRefs, diRefs, fast, resultNote, 'comb_SA');
+runner.run();
+
+for i = 1:5
+	params.infl.downMult = 1 - i*0.2;
+	runner = CombRunner(user, imposter, params, probeSets, setType, ...
+		monoRefs, diRefs, fast, resultNote, 'comb_SA');
+	runner.run();
+end
+end
